3f50531ec12194ae38266eb95f0d3d54d458f8e0
Insight Team <muhammadfaiz2603@gmail.com>
2026-01-12 15:09:42 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,7 +20,7 @@ st.markdown("---")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("🥇")
+    st.header("🥉")
     st.subheader("Explorer")
     st.write("Discovery and First Steps")
     st.link_button("Go to Level 1", "https://level-1-ssc-2026-vde8mt7hhorr5zgom3ac9e.streamlit.app", use_container_width=True)
@@ -32,7 +32,7 @@ with col2:
     st.link_button("Go to Level 2", "https://level-2-ssc-2026-cz4pctz4ls96suuku3quuc.streamlit.app/", use_container_width=True)
 
 with col3:
-    st.header("🥉")
+    st.header("🥇")
     st.subheader("Master")
     st.write("Legacy and Ultimate Challenge")
     st.link_button("Go to Level 3", "https://level-3-ssc-2026-2texh4j97wnqc4awh9po7k.streamlit.app/", use_container_width=True)
