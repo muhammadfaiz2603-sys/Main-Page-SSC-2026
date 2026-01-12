2d7e51f05b05d7f1b9f5d1254c6644dd11e3487e
muhammadfaiz2603-sys <muhammadfaiz2603@gmail.com>
2026-01-12 14:33:18 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,6 +8,7 @@ st.set_page_config(
 )
 
 # 2. Main Header and Styling
+st.image("Main Banner.png", use_container_width=True)
 st.title(" Switch Staff Challenge 2026")
 st.markdown("### Dashboard Access Portal")
 st.write("Please select a performance level below to view the detailed analytics.")
